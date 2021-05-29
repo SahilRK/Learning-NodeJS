0318880e79cf95b4b1841c3aeabf202b7a7dfd6f
Sahil_Kulkarni <sahil.rordev@gmail.com>
2021-05-29 23:29:27 +0530

    Added code on JSON data addition retrival formatting and code to add and remove notes

--- Notes-App/notes.js
+++ Notes-App/notes.js
@@ -1,5 +1,67 @@
+const fs = require('fs');
+
 const getNotes = () => {
     return "Your notes..."
 }
 
-module.exports = getNotes;
+const loadNotes = () => {
+    try{
+        const dataBuffer = fs.readFileSync('notes.json');
+        //Convert from binary to string value
+        const dataJSON = dataBuffer.toString();
+        return JSON.parse(dataJSON);
+    }catch(e){
+        return [];
+    }
+}
+
+const addNote = (title,body) => {
+    const notes = loadNotes();
+
+    //Check for duplicate notes and return an error message if the note is already taken
+    const duplicateNotes = notes.filter((note) => {
+        return note.title === title
+    })
+
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes);
+        console.log('Note has been added!');
+    }else{
+        console.log("The note title is already taken! Please add the note with a new title")
+    }
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes);
+    fs.writeFileSync('notes.json',dataJSON);
+}
+
+const removeNotes = (title) =>{
+    const notes = loadNotes();
+    
+    const noteIndex = notes.findIndex((note) => {
+        return note.title === title
+    })
+
+    if(noteIndex !== -1){
+        notes.splice(noteIndex,1);
+        console.log(`Note with title ${title} has been removed successfully`);
+        
+        //Restringify the formatted notes array to JSON and write back to file
+        saveNotes(notes);
+
+    }else{
+        console.log("Note requested to be deleted has not been found!");
+    }
+}
+
+//When you want to export multiple methods or properties from one file to another, we pass an object.
+module.exports = {
+    addNote: addNote,
+    getNotes: getNotes,
+    removeNotes: removeNotes
+}
