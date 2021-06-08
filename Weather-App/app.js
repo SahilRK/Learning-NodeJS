a11d866b4bc17dfc11d8641525ab688d926c3a2e
Sahil_Kulkarni <sahil.rordev@gmail.com>
2021-06-08 11:21:15 +0530

    Implemented callback chaining on weather and geocode and changed the functionality to functions for reusability

--- Weather-App/app.js
+++ Weather-App/app.js
@@ -1,46 +1,22 @@
-const request = require('postman-request');
+const geocode = require('./utils/geocode.js');
+const weather = require('./utils/forecast');
 
-//Weather
-/* const weather_url = "http://api.weatherstack.com/current?access_key=3920b29c3ecb693b904c073e28a23683&query=Bengaluru";
+//Geocoding function
 
-//Request accepts two parameters. 1) The API options in the form of an object. 2) A callback function to work on the data received from the response to the request.
-request(
-    {
-        url:weather_url, 
-        json:true     
-    },(error,response)=> {
-        if(error){
-            console.log("Unable to connect to the weather service. Check your network or the URL")
-        }
-        else if(response.body.error){
-            console.log("Unable to find the location")
-        }
-        else{
-            const data = response.body.current;
-            console.log(`It is currently ${data.temperature}. It feels like ${data.feelslike} degrees out.`)
-            console.log(response);
-        }
-}); */
-
-//Geocoding
-const map_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Bengaluru.json?access_token=pk.eyJ1Ijoic2FoaWxyayIsImEiOiJja3BkdTR1ZHgwYTJjMm9vMWFqaXN6NWtlIn0.haVSarijEz0glyWBduj8bQ&limit=1"
-
-request(
-    {
-        url: map_url,
-        json: true
-    },(error,response) =>{
-        if(error){
-            console.log("Unable to connect to the geocoding service. Check your network or the URL")
-        }
-        else if(response.body.features.length === 0){
-            console.log("Unable to get location coordinates for the specified location")
-        }
-        else{
-            const data = response.body.features[0];
-            const latitude = data.center[1];
-            const longitude = data.center[0];
-            console.log(`Latitude is ${latitude} and Longitude is ${longitude}`);
-        }
+const city = process.argv[2];
+geocode.getLocationDetails(city,(geocodeError,geocodeData) => {
+    if(geocodeError){
+        //if there is an error here, the function returns back from this point and the execution stops
+        return console.log('Error',geocodeError);
     }
-);
+
+    weather.getWeatherDetails(geocodeData.location,(forecastError,forecastData) => {
+        if(forecastError){
+            return console.log('Error',forecastError);
+        }
+
+        console.log('Message from Geocode',geocodeData.message);
+        console.log('Location is',geocodeData.location);        
+        console.log('Message from Forecast',forecastData.message);
+    });
+})
